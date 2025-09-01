2a34902326b72039a9b101734b82dda78ca3bf40
devcostafelipe <devcostafelipe@gmail.com>
2025-09-01 17:28:01 -0300

    base url add

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -14,7 +14,7 @@ describe('Orange HRM tests', () => {
 
 
   it('Login - Sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField ).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
@@ -24,7 +24,7 @@ describe('Orange HRM tests', () => {
   })
 
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
